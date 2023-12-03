9e981902f284f15d2494c3beafc5c7a02b4c982a
陈湛明 <chenzhanming04@foxmail.com>
2023-12-03 21:47:25 +0000

    Clean up

--- src/HtmlEscape.ts
+++ src/HtmlEscape.ts
@@ -3,15 +3,10 @@ export function htmlEscapeChar(c: string): string {
     if (c == '>') return '&gt;'
     if (c == '&') return '&amp;'
     if (c == '"') return '&quot;'
-    if (c == "'") return '&#039;'
     if (c == " ") return '&nbsp;'
     return c
 }
 
 export function htmlEscape(s: string): string {
-    let r = ""
-    for (let i = 0; i < s.length; i++) {
-        r += htmlEscapeChar(s.charAt(i))
-    }
-    return r
+    return s.split("").map(htmlEscapeChar).join("")
 }
--- src/TextWithImageBackground.ts
+++ src/TextWithImageBackground.ts
@@ -1,4 +1,5 @@
 import {htmlEscapeChar} from "./HtmlEscape";
+import BreakBasedSplitter from "./BreakBasedSplitter";
 
 export default class TextWithImageBackground {
     text: string
@@ -12,7 +13,7 @@ export default class TextWithImageBackground {
         this.background = background
     }
 
-    generateHash(): string {
+    private generateHash(): string {
         if (TextWithImageBackground.hashCounterLock) {
             throw new Error("This is a rare error. Please mount slowly.")
         }
@@ -28,62 +29,35 @@ export default class TextWithImageBackground {
         return hash
     }
 
-    mount(parent: HTMLElement) {
-        const hash = this.generateHash()
-        const className = "text-with-image-background-" + hash
-
-        const element = document.createElement("div")
-        element.className = className
-        for (let i = 0; i < this.text.length; i += 1) {
-            const innerDiv = document.createElement("div")
-            innerDiv.innerHTML = htmlEscapeChar(this.text.charAt(i))
-            element.appendChild(innerDiv)
-        }
-
-        const style = `
-            .${className} {
-                display: flex;
-            }`
-
-        const innerStyle = `
-            .${className} > div {
-                width: fit-content;
-                -webkit-background-clip: text;
-                background-clip: text;
-                -webkit-text-fill-color: transparent;
-                background-size: 100% 100%;
-                background-image: url("${this.background}");
-            }`
-
-        parent.appendChild(element)
-        document.styleSheets[0].insertRule(style, 0)
-        document.styleSheets[0].insertRule(innerStyle, 0)
-    }
-
     htmlCss(): [string, [string, string]] {
-        const hash = this.generateHash()
-        const className = "text-with-image-background-" + hash
+        const className = "text-with-image-background-" + this.generateHash()
+        const words = BreakBasedSplitter.split(this.text)
 
-        let html = `<div class="${className}">`
-        for (let i = 0; i < this.text.length; i += 1) {
-            html += `<div>${htmlEscapeChar(this.text.charAt(i))}</div>`
-        }
-        html += "</div>"
+        const html =
+            `<div class="${className}">` + words.map(word =>
+                "<div>" + word.map(c =>
+                    "<div>" +
+                        htmlEscapeChar(c) +
+                    "</div>").join("") +
+                "</div>").join("") +
+            "</div>"
 
-        const style = `
-            .${className} {
-                display: flex;
-            }`
+        const style = [
+            `.${className} > div {`,
+                `display: inline-block;`,
+            `}`,
+        ].join(" ").trim()
 
-        const innerStyle = `
-            .${className} > div {
-                width: fit-content;
-                -webkit-background-clip: text;
-                background-clip: text;
-                -webkit-text-fill-color: transparent;
-                background-size: 100% 100%;
-                background-image: url("${this.background}");
-            }`
+        const innerStyle = [
+            `.${className} > div > div {`,
+                `display: inline-block;`,
+                `-webkit-background-clip: text;`,
+                `background-clip: text;`,
+                `-webkit-text-fill-color: transparent;`,
+                `background-size: 100% 100%;`,
+                `background-image: url("${this.background}");`,
+            `}`,
+        ].join(" ").trim()
 
         return [html, [style, innerStyle]]
     }
