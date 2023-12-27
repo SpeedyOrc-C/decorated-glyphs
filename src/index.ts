9e3be221097ea0f0e7c8f7c3cb9258f15a95dae4
陈湛明 <chenzhanming04@foxmail.com>
2023-12-27 21:04:11 +0000

    Add isRightToLeft()

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import TextWithImageBackground from "./TextWithImageBackground";
 import BreakBasedSplitter from "./BreakBasedSplitter";
 import TextWithImageBackgroundConfig from "./TextWithImageBackgroundConfig";
 
+export function isRightToLeft(text: string): boolean {
+    return text.match(/[\u0590-\u05FF\u0600-\u06FF\u0700-\u074F\u0750-\u077F\uFB50-\uFDFF\uFE70-\uFEFF]/) != null
+}
+
 export {
     TextWithImageBackground,
     BreakBasedSplitter,
